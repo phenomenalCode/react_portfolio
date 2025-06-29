139b85e755f6c2211ab0c836f47da9270ce29ecf
phenomenalCode <gbreal50@gmail.com>
2025-06-29 16:46:23 +0200

    WEATHER APP TEXT

--- src/projects.jsx
+++ src/projects.jsx
@@ -22,7 +22,7 @@ export const projectData = [
   {
     id: 3,
     title: 'Weather App',
-    description: 'A simple weather forecast app. Tools, JS and Typescript and css',
+    description: 'A simple weather forecast app. It displays current weather and a 4-day forecast using the OpenWeatherMap API. Users can search cities or cycle through presets (Stockholm, Gothenburg, Oslo). It shows temperature, weather icons, sunrise/sunset times, and updates the background dynamically based on conditions. Weather data is cached using localStorage.',
     img: img3,
     github: 'https://github.com/phenomenalCode/weather-app',
   },
